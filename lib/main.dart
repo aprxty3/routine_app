10961afff95ba88ffa44ec9c876d5528c83665f4
aprxty3 <ajiprxty3@gmail.com>
2022-10-14 00:01:32 +0700

    update ui

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:isar/isar.dart';
 import 'package:routine_app/collecetion/category.dart';
 import 'package:routine_app/collecetion/routine.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:routine_app/screen/create_routine.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   final dir = await getApplicationSupportDirectory();
   final isar = await Isar.open(
     schemas: [RoutineSchema, CategorySchema],
@@ -17,16 +19,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: const Text('Hello World'),
+      debugShowCheckedModeBanner: false,
+      title: 'Routine App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Routine Apps'),
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const CreateRoutine(),
+              ),
+            ),
+            icon: const Icon(Icons.add_circle_outline_outlined),
           ),
-        ),
+        ],
       ),
     );
   }
